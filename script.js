74396870121123c6edb532552fefa3f45b1066f8
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-05-10 14:17:48 -0300

    commit script e css

---
+++ script.js
@@ -0,0 +1,13 @@
+//alert('Olá, teste!')
+function confirmar() {
+    var ca1 = document.getElementById('ad') 
+    var ca2 = document.getElementById('op') //puxando as variaveis do html
+    var ca = Number(ca1.value) //convertendo
+    var co = Number(ca2.value)
+    var hipotenusa = (co**2+ca**2)**(1/2)
+    var msg = document.getElementById('msg') //msg da tela
+    var img = document.getElementById('imagem') //imagem da calculadora
+    msg.innerHTML = `A medida da Hipotenusa (c²) é: ${hipotenusa.toFixed(2)}`
+    img.src = "../Calculadora/imagens/pitagoras.jpg"
+    
+}
